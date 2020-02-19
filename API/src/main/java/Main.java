4d9d0619f269dca5fd04a6bf3815fb871707a0f6
sosa edison <sosa_edison@hotmail.com>
2020-02-19 08:34:51 -0600

    Updated Documentation
    This also runs in VScode if you give the maven executable setting the full path to the main class executable file (Main.java).

--- API/src/main/java/Main.java
+++ API/src/main/java/Main.java
@@ -1,16 +1,15 @@
 import static spark.Spark.*;
 /**
  * Some documentation should go here...
- * Run this in one of the two following editors:
- * IntelliJ from JetBrains
- * 			OR
- * Eclipse.
- *
+ * 
+ * You can run this code in any IDE or IDE-like editors,
+ * Just be sure to know how to set up the necessary Maven
+ * and executable files if you use any other IDE/editor
+ * other than eclipse or IntelliJ from JetBrains.
+ * 
  * If you run into any errors, be sure to set the language
  * to type 8 - Lambda expressions and also be sure to
  * use java SDK 1.8 or higher.
- *
- *
  */
 public class Main {
 	public static void main(String[] args) {
