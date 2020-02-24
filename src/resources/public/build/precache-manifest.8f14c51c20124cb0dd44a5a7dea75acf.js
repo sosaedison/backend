7200bab83109644ecfdbb51c869f30723bbd077f
Sosa Edison <sosa_edison@hotmail.com>
2020-02-24 17:15:30 -0600

    Deploying landing page to server

---
+++ src/resources/public/build/precache-manifest.8f14c51c20124cb0dd44a5a7dea75acf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6c25cbf447759e9f2c139d660512b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35e71bbddf557fd0b1b0",
+    "url": "/static/css/main.b0e21c5d.chunk.css"
+  },
+  {
+    "revision": "241abb0a83e753575532",
+    "url": "/static/js/2.bcc72c2a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.bcc72c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e71bbddf557fd0b1b0",
+    "url": "/static/js/main.a8959a78.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  },
+  {
+    "revision": "d15013ce1f23b762841cfca9ef88cf24",
+    "url": "/static/media/AUTO-GARCON-3.d15013ce.png"
+  }
+]);
