b4f4d762fa0c475c84c6d2322ab8f9afbf3164f2
Sosa Edison <sosa_edison@hotmail.com>
2020-02-25 01:24:04 -0600

    fixed a static file pathing issue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,17 @@
 import spark.Request;
 import spark.Response;
 import spark.Route;
-import static spark.Spark.*;/**
- * Some documentation should go here...
- * 
- * You can run this code in any IDE or IDE-like editors,
- * Just be sure to know how to set up the necessary Maven
- * and executable files if you use any other IDE/editor
- * other than eclipse or IntelliJ from JetBrains.
- * 
- * If you run into any errors, be sure to set the language
- * to type 8 - Lambda expressions and also be sure to
- * use java SDK 1.8 or higher.
+import static spark.Spark.*;
+
+/**
+ * Spark Hello world
  */
 public class Main {
 	public static void main(String[] args) {
 		port(80);
 		staticFiles.location("/public/build"); //Sets the location of static files
 
-		get("/home",new Route() { // home route
+		get("/",new Route() { // home route
 			@Override
 			public Object handle(Request request, Response response) throws Exception {
 				response.type("text/html");
