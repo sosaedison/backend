07f912f10fec0fe6049889c369a89622f0bc9110
SupahAmbition <tylerbeverley@gmail.com>
2020-02-25 16:13:28 -0600

    add routes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,18 +33,52 @@ import static spark.Spark.*;
  * If you run into any errors, be sure to set the language
  * to type 8 - Lambda expressions and also be sure to
  * use java SDK 1.8 or higher.
+ *
+ *
+ * Notes:
+ * ------
+ *  Use a response transformer to transform handler's results into json. 
+ *  Spark 
+ *
+ *  import com.google.gson.Gson;
+ *  public class JsonTransformer implements ResponseTransformer {
+ *      private Gson gson = new Gson();
+ *       @Override
+ *      public String render(Object model) {
+ *          return gson.toJson(model);
+ *      }
+ *
+ * }
+ *
+ *
  */
 
 
-
 public class Main {
+
+
+    /*
+     * Can implement custom 501 handling in the future. 
+     */
+    public static void EndpointNotImplemented( Response res ){
+        res.status(501); 
+    }
+
+
 	public static void main(String[] args) {
 		
         port(80);
 
 		staticFiles.location("/public/build"); //Sets the location of static files
 
-		get("/",new Route() { // home route
+
+
+        /*
+         * Routes
+         *
+         */ 
+
+		get("/" ,new Route() { // home route
 			@Override
 			public Object handle(Request request, Response response) throws Exception {
 				response.type("text/html");
@@ -52,5 +86,35 @@ public class Main {
 				return null;
 			}
 		});
+
+        path("/api", () -> {
+            path("/users", () -> {
+                post("/newuser", EndpointNotImplemented); 
+                path("/:userid", () -> {
+                    get("", EndpointNotImplemted);
+                    get("/favorites", EndpointNotImplmented);
+                    get("/orders", EndpointNotImplemented); 
+                });
+            });
+            path("/restaurant", () -> {
+                path("/:resturantid", () -> {
+                    get("", EndpointNotImplemented); 
+                    get("/orders", EndpointNotImplemented );
+                    get("/tables", EnpointNotImplemented ); 
+                    post("/sitdown", EndpointNotImplemented ); 
+                    post("/orders/submit", EndpointNotImplemented); 
+                    post("orders/complete", EndpointNotImplemented); 
+                    path("/menu", () -> {
+                        get("", EndpointNotImplemented ); 
+                        post("/add", EndpointNotImplemented ); 
+                        post("/remove", EndpointNotImplemented); 
+                    });
+                });
+            });
+        });
+
+
+
+
 	}
 }
