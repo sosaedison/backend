0d32037aaf5b88824aafef94b461db98e3af0acd
Tyler Beverley <tylerbeverley@gmail.com>
2020-03-18 17:06:08 -0500

    Adjust menu item to db changes

--- src/main/java/MenuItem.java
+++ src/main/java/MenuItem.java
@@ -1,6 +1,11 @@
 package AutoGarcon; 
 import com.google.gson.Gson; 
 import com.google.gson.JsonSyntaxException;
+import com.google.gson.JsonElement; 
+import java.io.File;
+import java.sql.ResultSet; 
+import java.util.ArrayList; 
+import java.sql.SQLException; 
 
 /**
  * @author Tyler Beverley 
@@ -20,12 +25,14 @@ public class MenuItem {
         OTHER 
     }
 
+    private int itemID; 
     private Allergen allergens[]; 
     private String category; 
     private String description; 
     private String imageBytes;
     private String name; 
     private float price; 
+    private transient File image; 
 
     public static MenuItem menuItemFromJson( String body ) {
         Gson gson = new Gson(); 
@@ -38,6 +45,67 @@ public class MenuItem {
                     "Request body: %s\n. Exception: %s\n", body, e.toString() );
         }
         return item; 
+    }
+
+    public MenuItem(){
+        this.itemID = -1; 
+        this.allergens = new Allergen[0]; 
+    }
+
+    public MenuItem( ResultSet rs ){
+
+        ArrayList<Allergen> allergens = new ArrayList<Allergen>(); 
+
+        try{ 
+            this.itemID = rs.getInt( "itemID" );  
+            this.category = rs.getString("category"); 
+            this.description = rs.getString("description"); 
+            this.name = rs.getString("itemName"); 
+            
+            if( rs.getBoolean("gluten") ){
+                allergens.add( Allergen.GLUTEN );
+            }
+            if( rs.getBoolean("meat") ){
+                allergens.add( Allergen.MEAT ); 
+            }
+            if( rs.getBoolean("diary") ){
+                allergens.add( Allergen.DIARY ); 
+            }
+            if( rs.getBoolean("nuts") ){
+                allergens.add( Allergen.NUTS ); 
+            }
+            if( rs.getBoolean("soy") ) {
+                allergens.add( Allergen.SOY ); 
+            }
+            this.allergens = (Allergen[])allergens.toArray();
+        } catch( SQLException e) {
+            System.out.printf("Failed to get the required fields while creating a menuItem object.\n" + 
+                    "Exception: %s\n", e.toString() );
+        }
+
+    }
+
+    public static MenuItem[] menuItems( int menuID ){
+
+        ResultSet rs = DBUtil.getMenuItems( menuID ); 
+        ArrayList<MenuItem> result = new ArrayList<MenuItem>(); 
+
+        try { 
+            while( rs.next() ){
+                MenuItem mItem = new MenuItem( rs ); 
+                result.add( mItem ); 
+            }
+        } catch( SQLException e ){
+            System.out.printf("Failed to get next row in result set.\n Exception: %s\n", e.toString() );
+            return null; 
+        }
+
+        return (MenuItem[]) result.toArray();
+    }
+
+    public void saveImage( int menuID ){
+        byte[] bytes = ImageUtil.deserialize( this.imageBytes );
+        this.image = ImageUtil.saveImage( menuID, this.itemID, bytes);   
     }
 
     @Override 
