079cad4ff7f3d8559609e4c014a8e2b3a7c0f9a7
Sosa Edison <sosa_edison@hotmail.com>
2020-03-26 22:21:04 -0500

    added passwordAuth and started session validator

---
+++ target/classes/public/precache-manifest.9b2c9708436f38e261d8f5acb15efca8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa1560a22ac021a763f6fc9c560a375",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4310fad3a41061c80ec2",
+    "url": "/static/css/main.da95a9b2.chunk.css"
+  },
+  {
+    "revision": "ba06a084011f4d31e549",
+    "url": "/static/js/2.aa2ecece.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.aa2ecece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4310fad3a41061c80ec2",
+    "url": "/static/js/main.b99fc6b0.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "1c6a85bb4f033a18a158cf47932866e1",
+    "url": "/static/media/AUTO-GARCON-4.1c6a85bb.jpeg"
+  }
+]);
